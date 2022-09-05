454a8875e08b6d36588881c0af00d6d68babfb36
FreeKill <32848668+TimHaase@users.noreply.github.com>
2022-09-05 12:27:06 +0200

    add rotation of the character and add the mapwalls

--- Tanks/Assets/Skripts/CharacterMovement.cs
+++ Tanks/Assets/Skripts/CharacterMovement.cs
@@ -6,30 +6,30 @@ public class CharacterMovement : MonoBehaviour
 {
 
     public float speed;
-    private CharacterController _characterController;
+    public float rotationSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController characterController;
+    Vector3 moveVector;
+    Vector3 turnVector;
+
+
+
+    void Awake()
     {
-        _characterController = gameObject.GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _characterController.Move(Movement() * Time.deltaTime);
+        var hInput =  Input.GetAxis("Horizontal");
+        var vInput = Input.GetAxis("Vertical");
+
+        moveVector = transform.forward * speed * vInput;
+        turnVector = transform.up * rotationSpeed * hInput;
+
+        characterController.Move(moveVector * Time.deltaTime);
+        transform.Rotate(turnVector * Time.deltaTime);
     }
 
-
-    Vector3 Movement()
-    {
-        Vector3 moveVector = Vector3.zero;
-
-        moveVector += transform.forward * Input.GetAxis("Vertical");
-        moveVector += transform.right * Input.GetAxis("Horizontal");
-
-        moveVector *= speed;
-
-        return moveVector;
-    }
 }
